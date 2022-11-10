b4f25e1a849cdf0fcb2bd504919166bdf8e992ec
omgyeti <agdurrette@gmail.com>
2022-11-10 04:24:41 -0500

    DB work
    Im learning, its ugly, its working.
    I need to get the info I need and pass it to another form, where the info will then get compiled into a page thats printable.

---
+++ dbconfig.py
@@ -0,0 +1,5 @@
+user=""
+password=""
+host="127.0.0.1"
+port="5432"
+database=""
--- server.py
+++ server.py
@@ -1,11 +1,26 @@
 from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
+import psycopg2
 import config
 @app.route('/',methods = ['POST', 'GET'])
 def index():
    if request.method == 'POST':
       uID = request.form['uID']
-      return render_template('template.html', uid=uID)
+      try:
+         connection = psycopg2.connect(user=config.dbconfig.user,password=config.dbconfig.password,host=config.dbconfig.host,port=config.dbconfig.port,database=config.dbconfig.database)
+         cursor = connection.cursor()
+         selectstatement = "SELECT uname, email FROM test.aaaaaaaaaaaaaaa WHERE userid = '"+uID+"'"
+         cursor.execute(selectstatement)
+         record = cursor.fetchone()
+         print(record)
+      finally:
+         if (connection):
+            cursor.close()
+            connection.close()
+         if record is None:
+            return render_template('index.html')
+         else:
+            return render_template('template.html', uid=record[0])
    else:
      return render_template('index.html')
 if __name__ == '__main__':
