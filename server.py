19fdf53a568cdcf00f23e46dc73a22e594bc3454
omgyeti <agdurrette@gmail.com>
2022-11-22 04:34:18 -0500

    Going a different direction for now
    Going a different direction for now, decided to start simple just to get things running.

--- server.py
+++ server.py
@@ -1,27 +1,15 @@
 from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 import psycopg2
-import config
-@app.route('/',methods = ['POST', 'GET'])
+@app.route('/',methods = ['GET'])
 def index():
+   return render_template('index.html')
+     
+@app.route('/parking-ticket',methods = ['POST', 'GET'])
+def ParkingTicket():
    if request.method == 'POST':
-      uID = request.form['uID']
-      try:
-         connection = psycopg2.connect(user=config.dbconfig.user,password=config.dbconfig.password,host=config.dbconfig.host,port=config.dbconfig.port,database=config.dbconfig.database)
-         cursor = connection.cursor()
-         selectstatement = "SELECT uname, email FROM test.aaaaaaaaaaaaaaa WHERE userid = '"+uID+"'"
-         cursor.execute(selectstatement)
-         record = cursor.fetchone()
-         print(record)
-      finally:
-         if (connection):
-            cursor.close()
-            connection.close()
-         if record is None:
-            return render_template('index.html')
-         else:
-            return render_template('template.html', uid=record[0])
+      return render_template('parking-ticket.html', uID=request.form['uID'])
    else:
-     return render_template('index.html')
+      return redirect(url_for('index'))
 if __name__ == '__main__':
-   app.run(port=config.port)
+   app.run(port=8080)
